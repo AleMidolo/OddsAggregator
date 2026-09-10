f71d9704b509a251c915e3c3d18099bd9bf20eb6
Alessandro Midolo <alemidolo@gmail.com>
2026-09-10 19:31:22 +0200

    feat: add Eplay24 OddsPapi connector package

---
+++ src/odds_aggregator/connectors/eplay24/__init__.py
@@ -0,0 +1,5 @@
+"""Eplay24 connector using the permitted OddsPapi API."""
+
+from .connector import Eplay24OddsPapiConnector, OddsPapiClient
+
+__all__ = ["Eplay24OddsPapiConnector", "OddsPapiClient"]
