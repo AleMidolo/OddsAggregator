c44ae04d7ab35de85fc2ac9868c823f750f1baec
Alessandro Midolo <alemidolo@gmail.com>
2026-09-11 07:46:23 +0200

    fix: export freshness-aware Eplay24 connector

--- src/odds_aggregator/connectors/eplay24/__init__.py
+++ src/odds_aggregator/connectors/eplay24/__init__.py
@@ -1,5 +1,6 @@
 """Eplay24 connector using the permitted OddsPapi API."""
 
-from .connector import Eplay24OddsPapiConnector, OddsPapiClient
+from .connector import OddsPapiClient
+from .freshness import Eplay24OddsPapiConnector
 
 __all__ = ["Eplay24OddsPapiConnector", "OddsPapiClient"]
